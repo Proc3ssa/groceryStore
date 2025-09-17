fc3a3904931592e64ca92b6e894af1e2fbdc6171
Faisal <pros3sa@gmail.com>
2025-09-17 14:05:36 +0000

    feat: add walktrough pages

--- src/app/features/welcome/walkthrough/walkthrough.component.ts
+++ src/app/features/welcome/walkthrough/walkthrough.component.ts
@@ -1,11 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-walkthrough',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './walkthrough.component.html',
   styleUrl: './walkthrough.component.scss'
 })
 export class WalkthroughComponent {
+constructor(private router: Router) { }
+
+  page1 = true;
+
+ 
+    ash = '#C4C4C4'
+    green = '#53B175'
+ 
+
+  details = 
+
+    {
+      "title": 'Easy Shopping & Fesh Finds At Your Fingertips',
+
+      "desc": 'Your one-stop shop for all your grocery needs. Discover a wide range of product',
+
+      "img": 'assets/images/womanbag.png'
+    }
+
+    next(){
+      this.page1 = false;
+
+      if(!this.page1){
+      this.details =  {
+      "title": 'Your Utility Grocery Experience Awaits You',
+
+      "desc": 'Your one-stop shop for all your grocery needs.',
+
+      "img": 'assets/images/womanshopping.png'
+    }
+
+    this.ash = '#53B175'
+    this.green = '#C4C4C4'
+    }
+
+    else{
+      this.router.navigate(['/login']);
+    }
+
+    }
+
+    skip(){
+      this.router.navigate(['/login']);
+    }
+
+     
+
+  
 
 }
