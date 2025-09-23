58198ba5af3206f537d1f5f487b18004b87cc8ed
Faisal <pros3sa@gmail.com>
2025-09-23 11:43:57 +0000

    feat: add select account type page

--- src/app/features/auth/account-type/account-type.component.ts
+++ src/app/features/auth/account-type/account-type.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-type',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './account-type.component.html',
   styleUrl: './account-type.component.scss'
 })
@@ -12,6 +13,13 @@ export class AccountTypeComponent {
   primaryColor = '#01AC66';
   white = '#d8d5d566';
   ash = '#bbbbbbff';
+  boxShadow = '0 0  8px rgba(0, 0, 2, 0.546)';
+  boxShadow2 = '';
 
 
+  selectAccountType() {
+
+  this.primaryColor = this.primaryColor === this.ash ? '#01AC66' : this.ash;
+
+  }
 }
