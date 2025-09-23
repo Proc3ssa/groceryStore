7408c7c6f6d1bda95d2b2b8641f074073ed6a72b
Faisal <pros3sa@gmail.com>
2025-09-23 11:47:05 +0000

    chore: add next button functionality

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { AccountTypeComponent } from './account-type/account-type.component';
-
+import { SignupComponent } from './signup/signup.component';
 export const authRoute: Routes = [
     {
         path: 'account-type', component: AccountTypeComponent
     },
+    {
+        path: 'signup', component: SignupComponent
+    }
 
     
 
