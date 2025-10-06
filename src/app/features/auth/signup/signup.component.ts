8658426950abfb84363b225ec1895d02b9f3f285
Faisal <pros3sa@gmail.com>
2025-10-06 10:33:41 +0000

    fix: resolve ngmodel error

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { InputFieldComponent } from '../../../shared/input-field/input-field.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [InputFieldComponent, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  username: string = '';
+  email: string = '';
+  password: string = '';
 }
--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-input-field',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.scss'
 })
 export class InputFieldComponent {
+
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() model: any;
+
+  @Output() modelChange = new EventEmitter<any>(); 
 
 }
